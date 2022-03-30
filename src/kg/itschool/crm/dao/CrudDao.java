9c160dcf5acff1f56a3d851ab280c220c44e5479
mruma <mr.umarov.1995@gmail.com>
2022-03-30 09:30:03 +0600

    CrudDao saveAll

--- src/kg/itschool/crm/dao/CrudDao.java
+++ src/kg/itschool/crm/dao/CrudDao.java
@@ -11,11 +11,12 @@ public interface CrudDao<Model> {
     Model save(Model model);
     Model findById(Long id);
     List<Model> findAll();
+    List<Model> saveAll(List<Model> models);
 
     default Connection getConnection() throws SQLException {
-        final String URL = "jdbc:postgresql://localhost:5432/crm";
-        final String USERNAME = "postgres";
-        final String PASSWORD = "'";
+        final String URL = "jdbc:postgresql://ec2-52-73-155-171.compute-1.amazonaws.com:5432/d45qoa3mssolpf";
+        final String USERNAME = "voweidmihtnvqw";
+        final String PASSWORD = "4068bc9c7437dc12b9b08de55943a5a02642a16c8a487dc512f67408062b8643";
 
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
